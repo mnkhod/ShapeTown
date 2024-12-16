a081f0a6534837cd03ba49affc4de95583d6d04f
batotgon602 <otgoo.dev@gmail.com>
2024-12-16 18:06:48 +0800

    PlayerIdle Animation in 4 direction and hitbox changes

--- src/game/prefabs/PlayerPrefab.js
+++ src/game/prefabs/PlayerPrefab.js
@@ -12,7 +12,8 @@ export default class PlayerPrefab extends Phaser.GameObjects.Container {
 
 		scene.physics.add.existing(this, false);
 		this.body.allowGravity = false;
-		this.body.setSize(28, 55, false);
+		this.body.setSize(24, 12, false);
+        this.body.setOffset(0, 40);
 
 		// skin
 		const skin = scene.add.sprite(14.53150085424582, 27.318725443132376, "player", 0);
@@ -37,7 +38,7 @@ export default class PlayerPrefab extends Phaser.GameObjects.Container {
 		this.outfit = outfit;
 
 		/* START-USER-CTR-CODE */
-		// Write your code here.		
+		// Write your code here.
 
 		const cam = scene.cameras.main;
 		cam.startFollow(this, true, 0.1, 0.1);
@@ -65,8 +66,7 @@ export default class PlayerPrefab extends Phaser.GameObjects.Container {
 	/** @type {Phaser.Physics.Arcade.Container} */
 	player;
 
-
-	// Lifecycle Functions
+	lastDirection = 'down';
 
 	prefabCreateCycle() {
 		this.settingUpAnimations();
@@ -75,8 +75,6 @@ export default class PlayerPrefab extends Phaser.GameObjects.Container {
 	prefabUpdateCycle() {
 		this.playerMovement()
 	}
-
-	// Helper Functions
 
     playerMovement() {
 		let input = this.scene.input;
@@ -87,20 +85,20 @@ export default class PlayerPrefab extends Phaser.GameObjects.Container {
 		const rightKey = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
         if (rightKey.isDown) {
+            this.lastDirection = 'right';
             this.moveInDirection('right');
         } else if (leftKey.isDown) {
+            this.lastDirection = 'left';
             this.moveInDirection('left');
         } else if (upKey.isDown) {
+            this.lastDirection = 'up';
             this.moveInDirection('up');
         } else if (downKey.isDown) {
+            this.lastDirection = 'down';
             this.moveInDirection('down');
         } else {
-            this.moveInDirection('idle');
+            this.moveInDirection(`idle${this.capitalizeFirstLetter(this.lastDirection)}`);
         }
-
-        // Keep sprites aligned
-        // this.playerOutfit.setPosition(0, 0);
-        // this.playerHair.setPosition(0, 0);
     }
 
     moveInDirection(direction) {
@@ -110,29 +108,43 @@ export default class PlayerPrefab extends Phaser.GameObjects.Container {
             switch (direction) {
                 case 'right':
                     this.player.body.velocity.x = this.moveSpeed;
-
+                    this.player.body.velocity.y = 0;
                     this.playAnimations('Right');
                     break;
                 case 'left':
-					this.player.body.velocity.x = -this.moveSpeed;
-
+                    this.player.body.velocity.x = -this.moveSpeed;
+                    this.player.body.velocity.y = 0;
                     this.playAnimations('Left');
                     break;
                 case 'up':
                     this.player.body.velocity.y = -this.moveSpeed;
-
+                    this.player.body.velocity.x = 0;
                     this.playAnimations('Up');
                     break;
                 case 'down':
                     this.player.body.velocity.y = this.moveSpeed;
-
+                    this.player.body.velocity.x = 0;
                     this.playAnimations('Down');
                     break;
-                case 'idle':
-					this.player.body.velocity.x = 0;
-					this.player.body.velocity.y = 0;
-
-                    this.playAnimations('Idle');
+                case 'idleRight':
+                    this.player.body.velocity.x = 0;
+                    this.player.body.velocity.y = 0;
+                    this.playAnimations('IdleRight');
+                    break;
+                case 'idleLeft':
+                    this.player.body.velocity.x = 0;
+                    this.player.body.velocity.y = 0;
+                    this.playAnimations('IdleLeft');
+                    break;
+                case 'idleUp':
+                    this.player.body.velocity.x = 0;
+                    this.player.body.velocity.y = 0;
+                    this.playAnimations('IdleUp');
+                    break;
+                case 'idleDown':
+                    this.player.body.velocity.x = 0;
+                    this.player.body.velocity.y = 0;
+                    this.playAnimations('IdleDown');
                     break;
             }
         } catch (error) {
@@ -154,13 +166,15 @@ export default class PlayerPrefab extends Phaser.GameObjects.Container {
         let movementAnimKeys = {
             right: { start: 13, end: 17 },
             left: { start: 19, end: 23 },
-            idle: { start: 0, end: 0 },
             up: { start: 7, end: 9 },
-            down: { start: 1, end: 5 }
+            down: { start: 1, end: 5 },
+            idleRight: { start: 12, end: 12 },
+            idleLeft: { start: 18, end: 18 },
+            idleUp: { start: 6, end: 6 },  
+            idleDown: { start: 0, end: 0 }   
         };
 
-        // Add body animations
-        ["right", "left", "idle", "up", "down"].forEach((i) => {
+        ["right", "left", "up", "down", "idleRight", "idleLeft", "idleUp", "idleDown"].forEach((i) => {
             if (!this.scene.anims.exists(`body${this.capitalizeFirstLetter(i)}`)) {
                 this.scene.anims.create({
                     key: `body${this.capitalizeFirstLetter(i)}`,
@@ -174,14 +188,13 @@ export default class PlayerPrefab extends Phaser.GameObjects.Container {
             }
         });
 
-        // Add hair and outfit animations
         let anims = [
             { key: "hair", assetKey: "PlayerHair01" },
             { key: "outfit", assetKey: "PlayerOutfit01" },
         ];
 
         anims.forEach((anim) => {
-            ["right", "left", "idle", "up", "down"].forEach((i) => {
+            ["right", "left", "up", "down", "idleRight", "idleLeft", "idleUp", "idleDown"].forEach((i) => {
                 const animKey = `${anim.key}${this.capitalizeFirstLetter(i)}`;
                 if (!this.scene.anims.exists(animKey)) {
                     this.scene.anims.create({
