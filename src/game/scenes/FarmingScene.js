6afc0515fa6e154b34444d52f99d89a01cf7de5a
batotgon602 <otgoo.dev@gmail.com>
2024-12-17 15:28:22 +0800

    FarmingScene Refactor

--- src/game/scenes/FarmingScene.js
+++ src/game/scenes/FarmingScene.js
@@ -44,80 +44,79 @@ export default class FarmingScene extends Phaser.Scene {
 		farmingMap.addTilesetImage("Tree_v02", "Tree_v02");
 		farmingMap.addTilesetImage("Tree_v014", "Tree_v014");
 		farmingMap.addTilesetImage("Fruitbushes_V01", "Fruitbushes_V01");
-		farmingMap.addTilesetImage("FarmHorseStable", "FarmHorseStable");
 		farmingMap.addTilesetImage("NPCShopStandMaerchant", "NPCShopStandMaerchant");
 		farmingMap.addTilesetImage("ShopInteriorObjects_", "ShopInteriorObjects_");
 		farmingMap.addTilesetImage("StoneFance.", "StoneFance.");
 
 		// render_BackGround_1
-		const render_BackGround_1 = farmingMap.createLayer("Render/BackGround", ["RoadStone"], -768, -416);
+		const render_BackGround_1 = farmingMap.createLayer("Render/BackGround", ["GroundTileset","RoadStone"], -864, -496);
 
 		// render_RoadStone_JustRender__1
-		const render_RoadStone_JustRender__1 = farmingMap.createLayer("Render/RoadStone[JustRender]", ["RoadStone"], -768, -416);
+		const render_RoadStone_JustRender__1 = farmingMap.createLayer("Render/RoadStone[JustRender]", ["RoadStone"], -864, -496);
 
 		// render_FenceWooden_Make_a_Collider__1
-		const render_FenceWooden_Make_a_Collider__1 = farmingMap.createLayer("Render/FenceWooden[Make a Collider]", ["RoadStone"], -768, -416);
-
-		// removeable_Assets_1
-		const removeable_Assets_1 = farmingMap.createLayer("Removeable Assets", ["Flower_v02","Flower_v01","Flower_v03","Flower_v04","GroundAccessor","CityHouses_v02","RoadStone","Flower_v05"], -768, -416);
+		const render_FenceWooden_Make_a_Collider__1 = farmingMap.createLayer("Render/FenceWooden[Make a Collider]", ["RoadStone"], -864, -496);
 
 		// dEVS_IMPORTANT____Soil_Are_which_can_plain_crop_on_top__1
-		const dEVS_IMPORTANT____Soil_Are_which_can_plain_crop_on_top__1 = farmingMap.createLayer("DEVS IMPORTANT!!!/Soil[Are which can plain crop on top]", ["GroundTilestSoil"], -768, -416);
+		const dEVS_IMPORTANT____Soil_Are_which_can_plain_crop_on_top__1 = farmingMap.createLayer("DEVS IMPORTANT!!!/Soil[Are which can plain crop on top]", ["GroundTilestSoil"], -864, -496);
+
+		// dEVS_IMPORTANT____Removeable_Assets_1
+		const dEVS_IMPORTANT____Removeable_Assets_1 = farmingMap.createLayer("DEVS IMPORTANT!!!/Removeable Assets", ["Flower_v02","Flower_v01","Flower_v03","Flower_v04","GroundAccessor","CityHouses_v02","RoadStone","Flower_v05"], -864, -496);
 
 		// treeBorder_TreeL
-		const treeBorder_TreeL = farmingMap.createLayer("TreeBorder/TreeL7", ["TiletTreePattern"], -768, -416);
+		const treeBorder_TreeL = farmingMap.createLayer("TreeBorder/TreeL7", ["TiletTreePattern"], -864, -496);
 
 		// treeBorder_TreeL_1
-		const treeBorder_TreeL_1 = farmingMap.createLayer("TreeBorder/TreeL6", ["TiletTreePattern"], -768, -416);
+		const treeBorder_TreeL_1 = farmingMap.createLayer("TreeBorder/TreeL6", ["TiletTreePattern"], -864, -496);
 
 		// treeBorder_StoneFance_1
-		const treeBorder_StoneFance_1 = farmingMap.createLayer("TreeBorder/StoneFance", ["StoneFance."], -768, -416);
+		const treeBorder_StoneFance_1 = farmingMap.createLayer("TreeBorder/StoneFance", ["StoneFance."], -864, -496);
 
 		// treeBorder_TreeL_2
-		const treeBorder_TreeL_2 = farmingMap.createLayer("TreeBorder/TreeL4", ["TiletTreePattern"], -768, -416);
+		const treeBorder_TreeL_2 = farmingMap.createLayer("TreeBorder/TreeL4", ["TiletTreePattern"], -864, -496);
 
 		// treeBorder_TreeL_3
-		const treeBorder_TreeL_3 = farmingMap.createLayer("TreeBorder/TreeL3", ["TiletTreePattern"], -768, -416);
+		const treeBorder_TreeL_3 = farmingMap.createLayer("TreeBorder/TreeL3", ["TiletTreePattern"], -864, -496);
 
 		// treeBorder_TreeL_4
-		const treeBorder_TreeL_4 = farmingMap.createLayer("TreeBorder/TreeL2", ["TiletTreePattern"], -768, -416);
+		const treeBorder_TreeL_4 = farmingMap.createLayer("TreeBorder/TreeL2", ["TiletTreePattern"], -864, -496);
 
 		// treeBorder_TreeL_5
-		const treeBorder_TreeL_5 = farmingMap.createLayer("TreeBorder/TreeL1", ["TiletTreePattern"], -768, -416);
+		const treeBorder_TreeL_5 = farmingMap.createLayer("TreeBorder/TreeL1", ["TiletTreePattern"], -864, -496);
 
 		// house_area_HouseUnderGround_1
-		const house_area_HouseUnderGround_1 = farmingMap.createLayer("House area/HouseUnderGround", ["GroundTileset"], -768, -416);
+		const house_area_HouseUnderGround_1 = farmingMap.createLayer("House area/HouseUnderGround", ["GroundTileset"], -864, -496);
 
 		// house_area_House_1
-		const house_area_House_1 = farmingMap.createLayer("House area/House", ["CityHouses_v02"], -768, -416);
+		const house_area_House_1 = farmingMap.createLayer("House area/House", ["CityHouses_v02"], -864, -496);
 
 		// house_area_chest_1
-		const house_area_chest_1 = farmingMap.createLayer("House area/chest", ["ShopInteriorObjects_"], -768, -416);
+		const house_area_chest_1 = farmingMap.createLayer("House area/chest", ["ShopInteriorObjects_"], -864, -496);
 
 		// group_5_Decoration_1
-		const group_5_Decoration_1 = farmingMap.createLayer("Group 5/Decoration", ["DecorationClouthHangger"], -768, -416);
+		const group_5_Decoration_1 = farmingMap.createLayer("Group 5/Decoration", ["DecorationClouthHangger"], -864, -496);
 
 		// group_5_Dead_treee_1
-		const group_5_Dead_treee_1 = farmingMap.createLayer("Group 5/Dead treee", ["Tree_v09","Tree_v02","Tree_v08","Tree_v014"], -768, -416);
+		const group_5_Dead_treee_1 = farmingMap.createLayer("Group 5/Dead treee", ["Tree_v09","Tree_v02","Tree_v08","Tree_v014"], -864, -496);
 
 		// group_5_StrawberryBush_1
-		const group_5_StrawberryBush_1 = farmingMap.createLayer("Group 5/StrawberryBush", ["Fruitbushes_V01"], -768, -416);
+		const group_5_StrawberryBush_1 = farmingMap.createLayer("Group 5/StrawberryBush", ["Fruitbushes_V01"], -864, -496);
 
 		// merchant_shopStand_1
-		const merchant_shopStand_1 = farmingMap.createLayer("Merchant/shopStand", ["FarmHorseStable"], -768, -416);
+		const merchant_shopStand_1 = farmingMap.createLayer("Merchant/shopStand", ["FarmHorseStable"], -864, -496);
 
 		// merchant_NPCMerchant_1
-		const merchant_NPCMerchant_1 = farmingMap.createLayer("Merchant/NPCMerchant", ["NPCShopStandMaerchant"], -768, -416);
+		const merchant_NPCMerchant_1 = farmingMap.createLayer("Merchant/NPCMerchant", ["NPCShopStandMaerchant"], -864, -496);
 
 		// playerPrefab
-		const playerPrefab = new PlayerPrefab(this, -321, 405);
+		const playerPrefab = new PlayerPrefab(this, -367, 506);
 		this.add.existing(playerPrefab);
 
 		this.render_BackGround_1 = render_BackGround_1;
 		this.render_RoadStone_JustRender__1 = render_RoadStone_JustRender__1;
 		this.render_FenceWooden_Make_a_Collider__1 = render_FenceWooden_Make_a_Collider__1;
-		this.removeable_Assets_1 = removeable_Assets_1;
 		this.dEVS_IMPORTANT____Soil_Are_which_can_plain_crop_on_top__1 = dEVS_IMPORTANT____Soil_Are_which_can_plain_crop_on_top__1;
+		this.dEVS_IMPORTANT____Removeable_Assets_1 = dEVS_IMPORTANT____Removeable_Assets_1;
 		this.treeBorder_TreeL = treeBorder_TreeL;
 		this.treeBorder_TreeL_1 = treeBorder_TreeL_1;
 		this.treeBorder_StoneFance_1 = treeBorder_StoneFance_1;
@@ -146,9 +145,9 @@ export default class FarmingScene extends Phaser.Scene {
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
 	render_FenceWooden_Make_a_Collider__1;
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
-	removeable_Assets_1;
-	/** @type {Phaser.Tilemaps.TilemapLayer} */
 	dEVS_IMPORTANT____Soil_Are_which_can_plain_crop_on_top__1;
+	/** @type {Phaser.Tilemaps.TilemapLayer} */
+	dEVS_IMPORTANT____Removeable_Assets_1;
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
 	treeBorder_TreeL;
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
@@ -204,13 +203,13 @@ export default class FarmingScene extends Phaser.Scene {
     	this.group_5_StrawberryBush_1.setCollisionBetween(0, 10000);
 		// this.group_5_StrawberryBush_1.renderDebug(this.add.graphics());
 
-    	this.physics.add.collider(this.playerPrefab, this.group_5_Decoration_1);
-    	this.group_5_Decoration_1.setCollisionBetween(0, 10000);
-		// this.group_5_Decoration_1.renderDebug(this.add.graphics());
-
     	this.physics.add.collider(this.playerPrefab, this.group_5_Dead_treee_1);
     	this.group_5_Dead_treee_1.setCollisionBetween(0, 10000);
 		// this.group_5_Dead_treee_1.renderDebug(this.add.graphics());
+
+    	this.physics.add.collider(this.playerPrefab, this.group_5_Decoration_1);
+    	this.group_5_Decoration_1.setCollisionBetween(0, 10000);
+		// this.group_5_Decoration_1.renderDebug(this.add.graphics());
 
     	this.physics.add.collider(this.playerPrefab, this.house_area_chest_1);
     	this.house_area_chest_1.setCollisionBetween(0, 10000);
@@ -251,6 +250,8 @@ export default class FarmingScene extends Phaser.Scene {
     	this.physics.add.collider(this.playerPrefab, this.render_FenceWooden_Make_a_Collider__1);
     	this.render_FenceWooden_Make_a_Collider__1.setCollisionBetween(0, 10000);
 		// this.render_FenceWooden_Make_a_Collider__1.renderDebug(this.add.graphics());
+
+
 	}
 
 	/* END-USER-CODE */
