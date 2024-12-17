4b3b269e04c375db7b927a0a43eccf3bd2f8ba43
Munkh-Od <mnkhod.dev@gmail.com>
2024-12-17 17:29:13 +0800

    Fixed Public Routing Issue

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -23,13 +23,13 @@ function Login() {
 
     return (
         <div className="">
-            <img src="/public/assets/ShapeTownBg.png" className='absolute top-0 left-0 z-0 object-cover h-full w-full' />
+            <img src="/assets/ShapeTownBg.png" className='absolute top-0 left-0 z-0 object-cover h-full w-full' />
 
             <div className='z-10 relative h-screen flex items-center ml-32'>
                 <div className='relative'>
-                    <img src="public/assets/shapeTownSign.png" className='relative' height={"920"} width={"705"} />
+                    <img src="/assets/shapeTownSign.png" className='relative' height={"920"} width={"705"} />
                     <button className='absolute bottom-6 left-0 w-full flex justify-center' onClick={() => { openLogin() }}>
-                        <img src="public/assets/loginConnect.png" />
+                        <img src="/assets/loginConnect.png" />
                     </button>
                 </div>
             </div>
