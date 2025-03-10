ed3d15a576e6dc86357a9c66fc8fca0dbf322c44
namkhai-dev <b.namhai21@gmail.com>
2025-03-10 12:35:36 +0800

    new prefabs and json fix

---
+++ src/game/prefabs/stone/SandStonePrefab_2.js
@@ -0,0 +1,28 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class SandStonePrefab_2 extends Phaser.GameObjects.Image {
+
+	constructor(scene, x, y, texture, frame) {
+		super(scene, x ?? 318, y ?? 373, texture || "RockOnSand_V03", frame ?? 0);
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
---
+++ src/game/prefabs/stone/SandStonePrefab_4.js
@@ -0,0 +1,28 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class SandStonePrefab_4 extends Phaser.GameObjects.Image {
+
+	constructor(scene, x, y, texture, frame) {
+		super(scene, x ?? 700, y ?? 360, texture || "RockOnSand_V09", frame ?? 0);
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
